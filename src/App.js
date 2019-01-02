599ad9e83d339c1df952a1d31104924cb3a8ed0d
edfeld <edfeld1@gmail.com>
2019-01-02 11:32:33 -0800

    I have the scores working

--- src/App.js
+++ src/App.js
@@ -1,27 +1,102 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import friends from "./friends.json";
+import "./App.css";
+
 
 class App extends Component {
+  
+  state = {
+    // setting this.state.friends to the friends json array
+    friends,
+    intScore: 0,
+    intTopScore: 0,
+    gameStatus: "Click an image to begin"
+  }
+
+  //filter this.state.friends for friends with an id not equal to the id being removed
+  arrayShuffle = function(arr) {
+    let output = arr;
+     
+    for (var i = output.length-1; i >=0; i--) {
+     
+        var randomIndex = Math.floor(Math.random()*(i+1)); 
+        var itemAtIndex = output[randomIndex]; 
+         
+        output[randomIndex] = output[i]; 
+        output[i] = itemAtIndex;
+    }
+    return output;
+  }
+
+  // Update game status
+  updateGameStatus = newStatus => {
+  this.setState({gameStatus: newStatus});
+  }
+
+  //Process the click.  
+  processGuess = id => {
+    console.log("The id: ", id)
+    let arrIdx = friends.findIndex(arr => arr.id === id);
+    console.log("arrIdx: ", arrIdx);
+    if (arrIdx > -1) {
+      if (friends[arrIdx].isClicked === 0) {
+        friends[arrIdx].isClicked = 1;
+        console.log("this.state.intScore before:", this.state.intScore)
+        let tempScore = this.state.intScore + 1;
+        this.setState({ intScore: tempScore}); 
+        console.log("friends[arrIdx]", friends[arrIdx]);
+        console.log("====>intScore: ", tempScore);
+        let tempTopScore = this.state.intTopScore;
+         (tempScore > tempTopScore)? this.setState({intTopScore: tempTopScore+1 }): console.log("");
+         this.updateGameStatus("Correct Guess!");
+      } else {
+        console.log("reset my score");
+        this.setState({ intScore: 0});
+        friends.forEach(friend => {
+          friend.isClicked = 0;
+        });
+        this.updateGameStatus("Wrong Guess!");
+      }
+    }
+
+    let newfriends = this.arrayShuffle(friends)
+    // const friends = this.state.friends.filter(friend => friend.id !== id);
+    this.setState({ friends: newfriends });
+    // console.log("hitting the remove function, friends: ", this.state.friends);
+
+  }
+  //filter this.state.friends for friends with an id not equal to the id being removed
+  // removeFriend = id => {
+  //   const friends = this.state.friends.filter(friend => friend.id !== id);
+  //   this.setState({ friends });
+    
+  //   console.log("hitting the remove function, friends: ", this.state.friends);
+
+  // }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    <Wrapper
+    gameStatus={this.state.gameStatus}
+    intScore={this.state.intScore}
+    intTopScore={this.state.intTopScore}
+            >
+        {this.state.friends.map(friend=>(
+          <FriendCard 
+          removeFriend={this.removeFriend}
+          processGuess={this.processGuess}
+          id={friend.id}
+          key={friend.id}
+          image={friend.image}
+          /> 
+          )
+        )
+        
+      }
+       
+       </Wrapper>
+    )
   }
 }
 
---
+++ src/components/Wrapper/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "./style.css";
+
+class Wrapper extends React.Component {
+  // state = {
+  //   gameStatus: "Click an image to begin"
+  // }
+
+  // updateGameStatus = newStatus => {
+  //   this.setState.gameStatus(newStatus);
+  // }
+
+  render() {
+  return <div>
+    <nav className="navbar navbar-expand-lg">
+    <ul className="navbar-nav mr-0">
+      <li className="nav-item"><a className="navbar-brand" href="/">Clicky Game</a></li>
+      <li className="nav-other">{this.props.gameStatus}</li>
+      <li className="nav-other">Score {this.props.intScore} | Top Score {this.props.intTopScore}</li>
+    </ul>
+    </nav>
+    <div className="jumbo-container mx-0">
+      <div className="jumbotron bg-dark text-light">
+        <h1 className="jumbo-header text-center">The Clicky Game</h1>
+        <h4 className="text-center">Click on an image to earn points, but don't click on any more than once!</h4>
+        
+      </div>
+    
+    </div>
+    <div className="wrapper">{this.props.children}</div>;
+  </div>
+  }
+}
+
+// function Wrapper(props) {
+//   return <div>
+//     <nav className="navbar navbar-expand-lg">
+//     <ul className="navbar-nav mr-0">
+//       <li className="nav-item"><a className="navbar-brand" href="/">Clicky Game</a></li>
+//       <li className="nav-other">Click an image to begin</li>
+//       <li className="nav-other">Score {props.intScore} | Top Score {props.intTopScore}</li>
+//     </ul>
+//     </nav>
+//     <div className="jumbo-container mx-0">
+//       <div className="jumbotron bg-dark text-light">
+//         <h1 className="jumbo-header text-center">The Clicky Game</h1>
+//         <h4 className="text-center">Click on an image to earn points, but don't click on any more than once!</h4>
+        
+//       </div>
+    
+//     </div>
+//     <div className="wrapper">{props.children}</div>;
+//   </div>
+// }
+
+export default Wrapper;
