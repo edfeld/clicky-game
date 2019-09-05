131cf6ba07330bbfde03f3f61fdf3bb9999f8893
edfeld <edfeld1@gmail.com>
2019-09-04 21:34:14 -0700

    Updates

---
+++ precache-manifest.7aa3f9eb495b88df0738343fe4ccdc2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cb375f8eec9256502431c34b2eb636b",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "1ffac71c426591c425bb",
+    "url": "/clicky-game/static/css/main.20dee0a7.chunk.css"
+  },
+  {
+    "revision": "8c2414e0d1783cc1281e",
+    "url": "/clicky-game/static/js/2.340f901c.chunk.js"
+  },
+  {
+    "revision": "1ffac71c426591c425bb",
+    "url": "/clicky-game/static/js/main.959d23ce.chunk.js"
+  },
+  {
+    "revision": "79ea0d5502c3a6ac0f74",
+    "url": "/clicky-game/static/js/runtime~main.80bf01e5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clicky-game/precache-manifest.1ac8ecdf54696f5b1a25b2e519b8c206.js"
+  "/clicky-game/precache-manifest.7aa3f9eb495b88df0738343fe4ccdc2a.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/clicky-game/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clicky-game/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
